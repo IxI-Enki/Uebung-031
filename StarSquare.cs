91a349de1ed5ca79ce0bcf7ab5cfeee1cce79525
Enki <138018029+IxI-Enki@users.noreply.github.com>
2023-12-02 04:47:18 +0100

    Add files via upload

---
+++ StarSquare.cs
@@ -0,0 +1,73 @@
+﻿/*------------------------------------------------------------------------------
+ *                      HTBLA-Leonding / Class: 3ACIF
+ *------------------------------------------------------------------------------
+ *                      Jan Ritt
+ *------------------------------------------------------------------------------
+ *  Description:  The program draws a sqaure of stars, in the console center
+ *                with the side-lenght of user inputed size.
+ *------------------------------------------------------------------------------
+*/
+using System;
+using System.Threading;
+using System.Reflection.Metadata;
+
+namespace StarSquare
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      const int consoleWidth = 80;
+      const int consoleHeight = 30;
+      Console.SetWindowSize(consoleWidth, consoleHeight);
+
+      string userInput;
+      int userInteger;
+
+      Console.Write("\n                 Starsquare                 " +
+                    "\n============================================" +
+                    "\n Welche Seitenlänge soll das Quadrat haben? " +
+                    "\n ");
+      userInput = Console.ReadLine();
+      int.TryParse(userInput, out userInteger);
+
+      for (int i = 0; i < userInteger; i++)
+      {
+        for (int j = 0; j < userInteger; j++)
+        {
+          Console.Write("*");
+        }
+        Console.Write("\n");
+      }
+      Thread.Sleep(1000);
+      Console.Clear();
+
+      /// put output into center:
+      for (int i = 0; i < (consoleHeight - userInteger) / 2; i++)
+      {
+        Console.Write("\n");
+      }
+      for (int j = 0; j < userInteger; j++)
+      {
+        for (int k = 0; k < (consoleWidth - userInteger) / 2; k++)
+        {
+          Console.Write(" ");
+        }
+        for (int l = 0; l < userInteger; l++)
+        {
+          Console.Write("*");
+        }
+        Console.Write("\n");
+      }
+      
+      /// wait for a key to remove the square
+      Console.ReadLine();
+      Console.Clear();
+
+      Console.Write("\nZum Beenden bitte Eingabetaste drücken ...");
+      Console.ReadLine();
+      Console.Clear();
+
+    }
+  }
+}
